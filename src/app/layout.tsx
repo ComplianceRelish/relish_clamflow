d0f9b31c38db10adddbf6e9b3b3e1b207a21448e
Morpheus61 <motty.philip@gmail.com>
2026-01-03 06:32:53 +0530

    Fixed Explicit Add as image attributes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,6 +53,7 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="preload" href="/logo-relish.png" as="image" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
